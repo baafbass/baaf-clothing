cd7d96df10055dfb3c2a0a74a762f95865333fcd
Farid <abdoulfaridbassirou7898@gmail.com>
2024-03-04 20:23:34 +0300

    adding sass and categories scss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import './categories.styles.scss'
 const App = () => {
+  const categories = [
+  {
+    id:1,
+    title: 'Hats'
+  },
+   {
+    id:2,
+    title: 'Sneakers'
+  },
+   {
+    id:3,
+    title: 'Womens'
+  },
+   {
+    id:4,
+    title: 'Mens'
+  },
+   {
+    id:5,
+    title: 'Jackets'
+  }
+  ]
+
   return (
-    <div className="App">
-     Hello Guys
+    <div className="categories-container">
+    {categories.map(({title})=>(
+      <div className="category-container">
+      <div className="background-image"/>
+       <div className="category-body-container">
+         <h2>{title}</h2>
+         <p>Shop Now</p>
+       </div>
+     </div>
+      ))}
     </div>
   );
 }
