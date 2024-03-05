1b62b10edd5901f630755f6dac68fc94f4712f3a
Farid <abdoulfaridbassirou7898@gmail.com>
2024-03-05 17:31:30 +0300

    Create SignIn Route and install the firebase package

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,20 +1,24 @@
 
 import {Outlet,Link} from "react-router-dom"
 import {Fragment} from "react"
+import {ReactComponent as Logo} from "../../assets/crown.svg"
+import './navigation.styles.scss'
+
 
 const Navigation = () => {
   return(
      <Fragment>
      <div className="navigation">
       <Link className="logo-container" to="/">
-      <div> Logo </div>
+      <Logo className="logo"/>
       </Link>
       <div className="nav-links-container">
-
        <Link className="nav-link" to="/shop">
         SHOP
        </Link>
-
+       <Link className="nav-link" to="/sign-in">
+        SIGN IN
+       </Link>
       </div>
      </div>
      <Outlet />
---
+++ src/routes/sign-in/signIn.component.jsx
@@ -0,0 +1,10 @@
+
+const SignIn = () =>{
+	return(
+         <div>
+            Her is ther Sign In form
+         </div>
+		)
+}
+
+export default SignIn
