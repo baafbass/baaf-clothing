89d1cf1a20eeab4dc4b7b0a3962e4f2f12a1b283
Farid <abdoulfaridbassirou7898@gmail.com>
2024-03-07 14:25:00 +0300

    SignIn with getRedirectResult

--- src/routes/sign-in/signIn.component.jsx
+++ src/routes/sign-in/signIn.component.jsx
@@ -1,18 +1,9 @@
-
-import {useEffect} from 'react'
 import {getRedirectResult} from 'firebase/auth'
 
 import {auth,signInWithGooglePopup,createUserDocumentFromAuth,signInWithGoogleRedirect} from '../../utils/firebase/firebase.utils'
 
 const SignIn = () =>{
 
-   // useEffect(async ()=>{
-   //  const response = await getRedirectResult(auth);
-   // },[])
-
-   useEffect(async ()=>{
-   	const response = await getRedirectResult(auth);
-   })
 
 	const logGoogleUser = async () => {
       const {user} = await signInWithGooglePopup();
@@ -23,7 +14,6 @@ const SignIn = () =>{
          <div>
             <h1>Her is ther Sign In form</h1>
             <button onClick={logGoogleUser}>Sign In with Google Popup</button>
-            <button onClick={signInWithGoogleRedirect}>Sign In with Google Redirect</button>
          </div>
 		)
 }
