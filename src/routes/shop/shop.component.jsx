506364782ad1b8dd563390111a3a407621cde90c
Farid <abdoulfaridbassirou7898@gmail.com>
2024-03-11 23:32:35 +0300

    Creating the product Context

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,10 +1,13 @@
-import SHOP_DATA from '../../shop-data.json'
+import {useContext} from 'react'
+import {ProductsContext} from '../../contexts/products.context'
 
 const Shop = () => {
+
+    const {products} = useContext(ProductsContext)
 	return(
           <div>
           {
-          SHOP_DATA.map(({id,name})=>{
+          products.map(({id,name})=>{
           	return (
            <div key={id}>
            <h2>{name}</h2>
