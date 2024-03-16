2f19b53d8f6d8c059189b166b17a18d5a1442e63
Farid <abdoulfaridbassirou7898@gmail.com>
2024-03-16 22:20:32 +0300

    Finishing the Checkout part

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -32,12 +32,17 @@ const removeCartItem = (cartItems,cartItemToRemove) =>{
 		);
 }
 
+const clearCartItem = (cartItems,cartItemToClear) =>{
+ return cartItems.filter(cartItem => cartItem.id !== cartItemToClear.id)
+}
+
 export const CartContext = createContext({
 	isCartOpen:false,
 	setIsCartOpen: () => {},
 	cartItems: [],
 	addItemToCart: () => {},
 	removeItemToCart: ()=>{},
+	clearItemFromCart: ()=>{},
 	cartCount:0
 })
 
@@ -61,7 +66,19 @@ export const CartProvider = ({children}) => {
     	setCartItems(removeCartItem(cartItems,cartItemToRemove))
 }
 
-    const value = {isCartOpen,setIsCartOpen,cartItems,addItemToCart,cartCount,removeItemToCart}; 
+    const clearItemFromCart = (cartItemToClear) => {
+    	setCartItems(clearCartItem(cartItems,cartItemToClear))
+}
+
+    const value = {
+    	isCartOpen,
+    	setIsCartOpen,
+    	cartItems,
+    	addItemToCart,
+    	cartCount,
+    	removeItemToCart,
+    	clearItemFromCart
+    }; 
     
 	return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
