68e18282ca5f21d9381a1fb352e8c37c801c4bbc
Farid <abdoulfaridbassirou7898@gmail.com>
2024-03-19 00:01:37 +0300

    fixed the category problem

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,41 +1,32 @@
-import './category.styles.scss'
-import {useParams} from 'react-router-dom'
-import {useState,useEffect,useContext,Fragment} from 'react'
-import {CategoriesContext} from '../../contexts/categories.context'
-import ProductCard from "../../components/product-cart/product-cart.component"
+import './category.styles.scss';
+import { useParams } from 'react-router-dom';
+import { useState, useEffect, useContext, Fragment } from 'react';
+import { CategoriesContext } from '../../contexts/categories.context';
+import ProductCard from '../../components/product-cart/product-cart.component';
 
-const Category = () =>
-{
-  const {category} = useParams()
-  const {categoriesMap} = useContext(CategoriesContext)
-  const [products,setProducts] = useState(categoriesMap[category])
+const Category = () => {
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+  const [products, setProducts] = useState([]);
 
-  useEffect(()=>{
-    setProducts(categoriesMap[category])
-    console.log(products)
-     },[category,categoriesMap])
-
-  return
-  (
-    <Fragment>
-    <h2 className="category-title">{category.toUpperCase()}</h2>
-    <div className="category-container">
-    {
-      products && products.map((product)=>(<ProductCard key={product.id} product={product}/>))
+  useEffect(() => {
+    // Check if categoriesMap and category exist before setting products
+    if (categoriesMap && category && categoriesMap[category]) {
+      setProducts(categoriesMap[category]);
     }
-    </div>
+  }, [category, categoriesMap]);
+
+  return (
+    <Fragment>
+      <h2 className="category-title">{category && category.toUpperCase()}</h2>
+      <div className="category-container">
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
     </Fragment>
-
-   // <div className="category-container">
-   // {
-   // 	products &&
-   // 	products.map((product)=>
-   // 		(<ProductCard key={product.id} product={product}/>
-   // 	))
-   // }
-   // </div>
-  	)
-
-}
+  );
+};
 
 export default Category;
