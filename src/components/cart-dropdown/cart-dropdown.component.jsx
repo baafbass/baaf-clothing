7816ad573dff2df0d0a37421cccf8415a21d24b9
Farid <abdoulfaridbassirou7898@gmail.com>
2024-03-28 14:19:33 +0300

    Styled the Cart Dropdown Component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,4 @@
-import './cart-dropdown.styles.scss'
+import {CartDropdownContainer,EmptyMessage,CartItems} from './cart-dropdown.styles'
 import Button from '../button/button.component'
 import CartItem from '../cart-item/cart-item.component'
 import {useContext} from 'react'
@@ -15,18 +15,20 @@ const CartDropdown = () =>{
   }
 
   return(
-        <div className="cart-dropdown-container">
-        <div className="cart-items" >
-        {
-          cartItems.map((cartItem) => (
+        <CartDropdownContainer>
+        <CartItems>
+         {
+          cartItems.length ? (cartItems.map((cartItem) => (
             <CartItem 
             key={cartItem.id} 
             cartItem={cartItem}/> 
-            ))
-      }
-        </div>
+            ))) :  (
+                <EmptyMessage>Your cart is empty</EmptyMessage>
+          )
+         }
+        </CartItems>
         <Button onClick={goToCheckout}> CheckOut </Button>
-        </div>
+        </CartDropdownContainer>
   	)
 }
 
